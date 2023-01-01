05a029f90e76897edb442d50cb95be8c89861d49
entezarh173 <entezar.h173@gmail.com>
2022-12-31 23:55:50 -0800

    add files assets

---
+++ app/Http/Middleware copy/BlockFirefox.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use System\Middleware\Middleware;
+
+class BlockFirefox extends Middleware
+{
+    public function handle()
+    {
+        echo "scdsv";
+    }
+}
+ 
