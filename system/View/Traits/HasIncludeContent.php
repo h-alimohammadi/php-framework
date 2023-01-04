1066f98b79962336fee68f2572ea71cc0293fa11
entezarh173 <entezar.h173@gmail.com>
2023-01-03 18:45:26 -0800

    edit include content

--- system/View/Traits/HasIncludeContent.php
+++ system/View/Traits/HasIncludeContent.php
@@ -2,29 +2,28 @@
 
 namespace  System\View\Traits;
 
-trait HasIncludeContent{
+trait HasIncludeContent
+{
 
     private function checkIncludesContent()
     {
-       while(1)
-       {
-           $includesNamesArray = $this->findIncludesNames();
-           if(!empty($includesNamesArray)){
-               foreach($includesNamesArray as $includeName){
-                   $this->initialIncludes($includeName);
-               }
-           }
-           else{
-           break;
-           }
-       }
+        while (1) {
+            $includesNamesArray = $this->findIncludesNames();
+            if (!empty($includesNamesArray)) {
+                foreach ($includesNamesArray as $includeName) {
+                    $this->initialIncludes($includeName);
+                }
+            } else {
+                break;
+            }
+        }
     }
 
     private function findIncludesNames()
     {
         $includesNamesArray = [];
-       preg_match_all("/@include+\('([^)]+)'\)/", $this->content, $includesNamesArray, PREG_UNMATCHED_AS_NULL);
-       return isset($includesNamesArray[1]) ? $includesNamesArray[1] : false;
+        preg_match_all("/@include+\('([^)]+)'\)/", $this->content, $includesNamesArray, PREG_UNMATCHED_AS_NULL);
+        return isset($includesNamesArray[1]) ? $includesNamesArray[1] : false;
     }
 
     private function initialIncludes($includeName)
